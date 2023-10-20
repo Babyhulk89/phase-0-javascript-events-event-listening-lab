08131533cf4ace798ddd67b920569dac972410ba
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-10-19 21:11:53 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 function addingEventListener() {
 }
+// Get the button element by its ID
+const input = document.getElementById('button');
+
+// Define a callback function to be executed when the button is clicked
+function handleClick() {
+  alert('I was clicked!');
+}
+
+// Add the event listener to the button element
+input.addEventListener('click', handleClick);
+function addingEventListener() {
+    // Get the button element by its ID
+    const input = document.getElementById('button');
+  
+    // Define a callback function to be executed when the button is clicked
+    function handleClick() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener to the button element
+    input.addEventListener('click', handleClick);
+  }
+  
+  // Call the outer function to execute the event listener
+  addingEventListener();
+  
